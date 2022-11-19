e417d843bb08dfd9cd912968fe8c74bbea2b59b6
cola594223 <76999614+cola594223@users.noreply.github.com>
2022-11-19 15:52:14 +0800

    11/19 update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+var url = document.URL;
+
+if (url.split("#").length > 1 && url.split("#")[1] == 'example') {
+    showExample();
+}
+
 function showExample() {
     hideAll();
     $("#example").show();
